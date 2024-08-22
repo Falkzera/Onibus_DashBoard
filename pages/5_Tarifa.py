d3b4dd9385184d1baed9cb4998fb42e96ef790b8
falkzera <falkzera@gmail.com>
2024-08-22 11:17:01 -0300

    Update

--- pages/5_Tarifa.py
+++ pages/5_Tarifa.py
@@ -148,8 +148,19 @@ with st.expander('Comparação Acumulada do Gasto da Renda Anual'):
 ########################################################################################################################################
 
 
-with st.expander('COMPARAÇÃO DE GESTÃO COMPLETO'):
 
+# Configuração da página
+
+# Título
+st.title('Transporte Público - Prefeitura de Maceió :bus:')
+st.subheader("Comparativo do percentual de gastos anuais com transporte público em Maceió por gestão")
+with st.expander('Sobre o Projeto', expanded=True):
+    st.subheader('Objetivo:')
+    st.write('O objetivo central é visualizar o quanto que os cidadões Maceioenses precisam desembolsar anualmente, com base no salário minimo de cada época, para se locomover através do Transporte Público de Maceió. Foi considerado duas passagens por dia (Ida e Volta, de domingo a domingo) e adicionado o cenário de dependentes: (01 filho, 02 filhos e 03 filhos).')
+    st.write('Considerando o salário minimo de cada época, foi calculado o percentual do salário minimo que o cidadão precisaria desembolsar para se locomover através do transporte público de Maceió.')
+
+
+with st.container():
     # Carregar o novo DataFrame
     df_novo = pd.read_excel("data/GASTOS_GROUPED.xlsx")
     # Remover de PREFEITO as '[]' e as aspas
@@ -182,55 +193,132 @@ with st.expander('COMPARAÇÃO DE GESTÃO COMPLETO'):
 
     # Criar o gráfico de linha com ou sem pontos
     fig_gestao = px.line(df_gestao, x='ANO', y=coluna_selecionada, color='PREFEITO',
-                         labels={coluna_selecionada: f'Proporção do Gasto ({coluna_selecionada})', 'ANO': 'Ano de Gestão'},
-                         title=f'Proporção do gasto anual com transporte público possuíndo {coluna_selecionada} por gestão ao Longo dos Anos',
-                         markers=exibir_pontos)
+                        labels={coluna_selecionada: f'Proporção do Gasto ({coluna_selecionada})', 'ANO': 'Ano de Gestão'},
+                        title=f'Proporção do gasto anual com transporte público possuíndo {coluna_selecionada} por gestão ao Longo dos Anos',
+                        markers=exibir_pontos)
 
     # Adicionar anotações de texto para cada ponto, se a opção de exibir pontos estiver selecionada
     if exibir_pontos:
         for i in range(len(df_gestao)):
             fig_gestao.add_annotation(x=df_gestao['ANO'][i], y=df_gestao[coluna_selecionada][i],
-                                      text=f"{df_gestao[coluna_selecionada][i]:.2f}%",
-                                      showarrow=True, arrowhead=2, ax=0, ay=-20)
+                                    text=f"{df_gestao[coluna_selecionada][i]:.2f}%",
+                                    showarrow=True, arrowhead=2, ax=0, ay=-20)
+
+    # Atualizar a cor da linha para cada prefeito
+    fig_gestao.for_each_trace(
+        lambda trace: trace.update(line=dict(color='green')) if trace.name == 'JHC' else (
+            trace.update(line=dict(color='darkred')) if trace.name == 'KATIA BORN' else (
+                trace.update(line=dict(color='red')) if trace.name == 'CÍCERO ALMEIDA' else (
+                    trace.update(line=dict(color='darkorange')) if trace.name == 'RUI PALMEIRA' else ()
+                )
+            )
+        )
+    )
 
     fig_gestao.update_layout(xaxis_title=None, yaxis_title=None)
     st.plotly_chart(fig_gestao)
 
-# Adicionar um checkbox para exibir o DataFrame
-    df_novo = df_novo.sort_values(by=['ANO', 'PREFEITO'])
-    df_penultimo_ano = df_novo.groupby('PREFEITO').nth(-2).reset_index()
+# Adicionar um selectbox para escolher entre visualizar por prefeito ou por ano
+opcao_visualizacao = st.selectbox('Escolha a visualização', ['Por Prefeito', 'Por Ano'])
 
-    st.dataframe(df_penultimo_ano[['PREFEITO', '3 filhos', '2 filhos', '1 filho', 'sem filhos']], column_config={
+df_novo = df_novo.sort_values(by=['ANO', 'PREFEITO'])
+
+if opcao_visualizacao == 'Por Prefeito':
+    # Calcular a média dos indicadores por prefeito
+    df_media_prefeito = df_novo.groupby('PREFEITO').mean().reset_index()
+    
+    # Definir a ordem específica dos prefeitos
+    ordem_prefeitos = ['KATIA BORN', 'CÍCERO ALMEIDA', 'RUI PALMEIRA', 'JHC']
+    df_media_prefeito['PREFEITO'] = pd.Categorical(df_media_prefeito['PREFEITO'], categories=ordem_prefeitos, ordered=True)
+    df_media_prefeito = df_media_prefeito.sort_values('PREFEITO')
+    
+    # Exibir os dados tabulados por prefeito com colunas de progresso
+    st.dataframe(df_media_prefeito[['PREFEITO', '3 filhos', '2 filhos', '1 filho', 'sem filhos']], column_config={
         "3 filhos": st.column_config.ProgressColumn(
             "Gasto da Renda Anual (3 filhos)", 
             format="%.2f%%", 
             min_value=0, 
-            max_value=float(df_penultimo_ano['3 filhos'].max())
+            max_value=float(df_media_prefeito['3 filhos'].max())
         ),
         "2 filhos": st.column_config.ProgressColumn(
             "Gasto da Renda Anual (2 filhos)", 
             format="%.2f%%", 
             min_value=0, 
-            max_value=float(df_penultimo_ano['2 filhos'].max())
+            max_value=float(df_media_prefeito['2 filhos'].max())
         ),
         "1 filho": st.column_config.ProgressColumn(
             "Gasto da Renda Anual (1 filho)", 
             format="%.2f%%", 
             min_value=0, 
-            max_value=float(df_penultimo_ano['1 filho'].max())
+            max_value=float(df_media_prefeito['1 filho'].max())
         ),
         "sem filhos": st.column_config.ProgressColumn(
             "Gasto da Renda Anual (sem filhos)", 
             format="%.2f%%", 
             min_value=0, 
-            max_value=float(df_penultimo_ano['sem filhos'].max())
+            max_value=float(df_media_prefeito['sem filhos'].max())
         )
-    }, height=175, width=1200)
+    }, height=178, width=1500)
+else:
+    # Exibir os dados tabulados por ano com colunas de progresso
+    df_novo['ANO'] = df_novo['ANO'].astype(str).str.replace(',', '')
+    st.dataframe(df_novo[['ANO', 'PREFEITO', '3 filhos', '2 filhos', '1 filho', 'sem filhos']], column_config={
+        "3 filhos": st.column_config.ProgressColumn(
+            "Gasto da Renda Anual (3 filhos)", 
+            format="%.2f%%", 
+            min_value=0, 
+            max_value=float(df_novo['3 filhos'].max())
+        ),
+        "2 filhos": st.column_config.ProgressColumn(
+            "Gasto da Renda Anual (2 filhos)", 
+            format="%.2f%%", 
+            min_value=0, 
+            max_value=float(df_novo['2 filhos'].max())
+        ),
+        "1 filho": st.column_config.ProgressColumn(
+            "Gasto da Renda Anual (1 filho)", 
+            format="%.2f%%", 
+            min_value=0, 
+            max_value=float(df_novo['1 filho'].max())
+        ),
+        "sem filhos": st.column_config.ProgressColumn(
+            "Gasto da Renda Anual (sem filhos)", 
+            format="%.2f%%", 
+            min_value=0, 
+            max_value=float(df_novo['sem filhos'].max())
+        )
+    }, height=913, width=1500)
 
     st.write('De acordo com os dados, é nítido que a gestão do Prefeito JHC foi o que mais benéficiou os cidadão Maceioense com o transporte público, em comparação com as gestões anteriores.')
     st.write('Diversas políticas públicas foram empregadas durante esse período, como a implantação do Domingo é Livre e o Passa Gratuito para estudantes e diversas opções de integração entre linhas.')
 
-########################################################################################################################################
+# Expander para formato tabular por prefeito
+with st.expander('Formato tabular por prefeito'):
+    # Calcular a média dos indicadores por prefeito
+    df_media_prefeito = df_novo.groupby('PREFEITO').mean().reset_index()
+    
+    # Definir a ordem específica dos prefeitos
+    ordem_prefeitos = ['KATIA BORN', 'CÍCERO ALMEIDA', 'RUI PALMEIRA', 'JHC']
+    df_media_prefeito['PREFEITO'] = pd.Categorical(df_media_prefeito['PREFEITO'], categories=ordem_prefeitos, ordered=True)
+    df_media_prefeito = df_media_prefeito.sort_values('PREFEITO')
+    
+    # Formatar os valores com duas casas decimais e adicionar o sinal de %
+    df_media_prefeito[['3 filhos', '2 filhos', '1 filho', 'sem filhos']] = df_media_prefeito[['3 filhos', '2 filhos', '1 filho', 'sem filhos']].applymap(lambda x: f"{x:.2f}%")
+    
+    # Exibir os dados tabulados por prefeito sem a coluna de índice
+    st.table(df_media_prefeito[['PREFEITO', '3 filhos', '2 filhos', '1 filho', 'sem filhos']])
+
+# Expander para formato tabular por ano
+with st.expander('Formato tabular por ano'):
+    # Remover a vírgula dos anos
+    df_novo['ANO'] = df_novo['ANO'].astype(str).str.replace(',', '')
+    
+    # Formatar os valores com duas casas decimais e adicionar o sinal de %
+    df_novo[['3 filhos', '2 filhos', '1 filho', 'sem filhos']] = df_novo[['3 filhos', '2 filhos', '1 filho', 'sem filhos']].applymap(lambda x: f"{x:.2f}%")
+    
+    # Exibir os dados tabulados por ano
+    st.table(df_novo[['ANO', 'PREFEITO', '3 filhos', '2 filhos', '1 filho', 'sem filhos']])
+
+# créditos
 with st.sidebar:
     Credito.display_credits()
-
